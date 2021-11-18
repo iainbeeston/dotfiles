c4c77b1424ad5a7fa14fd88e7c6889bf4300b7d1
Iain Beeston <iainbeeston@users.noreply.github.com>
2021-11-18 15:23:19 +0000

    Added finicky config

---
+++ .finicky.js
@@ -0,0 +1,14 @@
+module.exports = {
+  defaultBrowser: "Safari",
+  handlers: [
+    {
+      match: [
+        "meet.google.com/*",
+      ],
+      browser: "Google Chrome"
+    }
+  ],
+  options: {
+    hideIcon: true
+  }
+}
